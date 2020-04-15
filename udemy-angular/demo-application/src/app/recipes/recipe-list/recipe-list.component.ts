9ffa6188bba0ffbacc218d93969d24e5e7c57df0
desonirh <rhdesonier@sbec.com>
2020-04-15 13:35:45 -0500

    Angular - Assignment 4

---
+++ udemy-angular/demo-application/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://c.pxhere.com/images/15/3d/9ee477ee62341b9480ce314b02f8-1417897.jpg!d'),
+    new Recipe('A Taco Recipe', 'This is simply a taco test', 'https://c.pxhere.com/photos/60/30/odyssey-354653.jpg!d')
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
