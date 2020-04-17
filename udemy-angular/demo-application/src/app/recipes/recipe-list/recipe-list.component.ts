17dff1d01b77ec3212f23c5cd25a3278a216793a
desonirh <rhdesonier@sbec.com>
2020-04-17 10:37:18 -0500

    Angular - Section 6

--- udemy-angular/demo-application/src/app/recipes/recipe-list/recipe-list.component.ts
+++ udemy-angular/demo-application/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -11,10 +11,14 @@ export class RecipeListComponent implements OnInit {
     new Recipe('A Test Recipe', 'This is simply a test', 'https://c.pxhere.com/images/15/3d/9ee477ee62341b9480ce314b02f8-1417897.jpg!d'),
     new Recipe('A Taco Recipe', 'This is simply a taco test', 'https://c.pxhere.com/photos/60/30/odyssey-354653.jpg!d')
   ];
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onRecipeSelected(recipe : Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
