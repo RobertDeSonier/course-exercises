252740dc8ce3eeb739e1ce09918837be03481b19
desonirh <rhdesonier@sbec.com>
2020-05-06 15:53:14 -0500

    Angular - Section 20

--- udemy-angular/demo-application/src/app/header/header.component.ts
+++ udemy-angular/demo-application/src/app/header/header.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DataStorageService } from '../shared/data-storage.service';
+import { AuthService } from '../auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
-  constructor(private storageService: DataStorageService) { }
+export class HeaderComponent implements OnInit, OnDestroy {
+  private userSub: Subscription;
+  isAuthenticated = false;
+
+  constructor(
+    private storageService: DataStorageService,
+    private authService: AuthService
+  ) { }
 
   ngOnInit(): void {
+    this.userSub = this.authService.user.subscribe(user => {
+      this.isAuthenticated = !!user;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.userSub.unsubscribe();
   }
 
   onSaveData() {
@@ -18,5 +33,9 @@ export class HeaderComponent implements OnInit {
 
   onFetchData() {
     this.storageService.fetchRecipes().subscribe();
+  }
+
+  onLogout() {
+    this.authService.logout();
   }
 }
